1f6487b79a644774a1493399659bcd4355139d14
Muhammad-Aman-Mirza <muhammad.aman@mtfxgroup.com>
2024-06-04 13:50:50 +0500

    feat: odd and even game without @Input() and @Output() decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CockpitComponent } from './cockpit/cockpit.component';
@@ -6,31 +6,40 @@ import { ServerElementComponent } from './server-element/server-element.componen
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CockpitComponent,ServerElementComponent,CommonModule,NzCardModule,NzLayoutModule,NzDropDownModule],
+  imports: [RouterOutlet,CockpitComponent,ServerElementComponent,CommonModule,NzCardModule,NzButtonModule,NzLayoutModule,NzDropDownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-practice';
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+  extraTemplate: string|TemplateRef<void>|undefined;
+  evenNumbers:number[] = []
+  oddNumbers:number[]  = []
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
+  // intervalFired!:number[]
+  intervelFired(firedNumber:number){
+    if(firedNumber%2 === 0){
+      this.evenNumbers.push(firedNumber)
+    }
+    else{
+      this.oddNumbers.push(firedNumber)
+    }
+  }
+  interval!:any
+  lastNumber = 0
+  startGame(){
+    this.interval = setInterval(()=>{
+      this.intervelFired(this.lastNumber+1)
+      this.lastNumber++
+    },1000)
   }
 
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    });
+  pauseGame(){
+    clearInterval(this.interval)
   }
+  
 
 }
