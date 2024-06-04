1e0f3c0329af14e524f2e120f106fa7dd89c22cc
Muhammad-Aman-Mirza <muhammad.aman@mtfxgroup.com>
2024-06-04 15:26:04 +0500

    feat: working on active and inactive users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,48 @@
-import { Component, TemplateRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { EvenComponent } from './even/even.component';
-import { OddComponent } from './odd/odd.component';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Component, TemplateRef } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { CockpitComponent } from "./cockpit/cockpit.component";
+import { ServerElementComponent } from "./server-element/server-element.component";
+import { GameControlComponent } from "./game-control/game-control.component";
+import { EvenComponent } from "./even/even.component";
+import { OddComponent } from "./odd/odd.component";
+import { NzCardModule } from "ng-zorro-antd/card";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzListModule } from "ng-zorro-antd/list";
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [OddComponent,EvenComponent,GameControlComponent,RouterOutlet,CockpitComponent,ServerElementComponent,CommonModule,NzCardModule,NzButtonModule,NzLayoutModule,NzDropDownModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  preserveWhitespaces:true
+  imports: [
+    OddComponent,
+    EvenComponent,
+    GameControlComponent,
+    RouterOutlet,
+    CockpitComponent,
+    ServerElementComponent,
+    CommonModule,
+    NzCardModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzDropDownModule,
+    NzListModule
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+  preserveWhitespaces: true
 })
 export class AppComponent {
-  extraTemplate: string|TemplateRef<void>|undefined;
-  evenNumbers:number[] = []
-  oddNumbers:number[]  = []
+  extraTemplate: string | TemplateRef<void> | undefined;
+  activeUsers = ["Max", "Anna"];
+  inactiveUsers = ["Chris", "Manu"];
 
-  // intervalFired!:number[]
-  intervelFired(firedNumber:number){
-    if(firedNumber%2 === 0){
-      this.evenNumbers.push(firedNumber)
-    }
-    else{
-      this.oddNumbers.push(firedNumber)
-    }
+  activeUser(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
   }
-
+  inactiveUser(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
 }
