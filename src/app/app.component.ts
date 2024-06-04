933203fd69af26d6a30e6fa4c623eae45bb42fc5
Muhammad-Aman-Mirza <muhammad.aman@mtfxgroup.com>
2024-06-04 14:17:22 +0500

    feat: odd and even game with @Input() and @Output() decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
@@ -10,7 +13,7 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CockpitComponent,ServerElementComponent,CommonModule,NzCardModule,NzButtonModule,NzLayoutModule,NzDropDownModule],
+  imports: [OddComponent,EvenComponent,GameControlComponent,RouterOutlet,CockpitComponent,ServerElementComponent,CommonModule,NzCardModule,NzButtonModule,NzLayoutModule,NzDropDownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   preserveWhitespaces:true
@@ -29,18 +32,5 @@ export class AppComponent {
       this.oddNumbers.push(firedNumber)
     }
   }
-  interval!:any
-  lastNumber = 0
-  startGame(){
-    this.interval = setInterval(()=>{
-      this.intervelFired(this.lastNumber+1)
-      this.lastNumber++
-    },1000)
-  }
-
-  pauseGame(){
-    clearInterval(this.interval)
-  }
-  
 
 }
